c731099393590950fb60917242863800206bcb9d
Alejandro Medina <5almero5@gmail.com>
2020-08-12 17:51:16 -0500

    Add Animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
-
+import { GlobalStyle } from './styles/GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 export const App = () => (
   <>
+    <Logo />
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
     border-radius: 10px;
@@ -10,6 +11,7 @@ export const ImgWrapper = styled.div`
     width: 100%;
 `
 export const Img = styled.img`
+    ${fadeIn()}
     box-shadow: 0 10px 14px rgba(0,0,0, 0.2);
     object-fit: cover;
     position: absolute;
