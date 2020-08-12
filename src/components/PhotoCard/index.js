eab16daabeaf69b5d3a3a89591b6167d1b177b84
Alejandro Medina <5almero5@gmail.com>
2020-08-12 17:23:49 -0500

    Add icons & photo list components

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icon'
+import { MdThumbUp } from 'react-icons/md'
+
 const DEFAULT_CARD = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
   return (
@@ -10,7 +11,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
           <Img src={src} />
         </ImgWrapper>
         <Button>
-          <MdFavoriteBorder size='32px' /> {likes} likes!
+          <MdThumbUp size='32px' /> {likes} likes!
         </Button>
       </a>
     </article>
