2bbe05247685e2b81828b2c1776c95bde639ad59
Alejandro Medina <5almero5@gmail.com>
2020-09-13 18:45:55 -0500

    Add hoock lazy load

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useRef, useEffect, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdThumbUp } from 'react-icons/md'
 
 const DEFAULT_CARD = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('Si')
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(ref.current)
+  })
+
   return (
-    <article>
+    <Article ref={ref}>
       <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-        <Button>
-          <MdThumbUp size='32px' /> {likes} likes!
-        </Button>
+        {show && <>
+          <ImgWrapper>
+            <Img src={src} />
+          </ImgWrapper>
+          <Button>
+            <MdThumbUp size='32px' /> {likes} likes!
+          </Button>
+                 </>}
       </a>
-    </article>
+    </Article>
   )
 }
