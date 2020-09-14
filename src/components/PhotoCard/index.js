55433e05260cb36a4a3b4e40ad697e77832fb473
Alejandro Medina <5almero5@gmail.com>
2020-09-13 21:15:59 -0500

    Add local storage api like

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,11 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdThumbUp } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_CARD = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
   const ref = useRef(null)
   const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      return false
+    }
+  })
 
   useEffect(function () {
     const observer = new window.IntersectionObserver(function (entries) {
@@ -19,7 +28,15 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
 
     observer.observe(ref.current)
   })
-
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
   return (
     <Article ref={ref}>
       <a href={`/detail/${id}`}>
@@ -27,8 +44,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
           <ImgWrapper>
             <Img src={src} />
           </ImgWrapper>
-          <Button>
-            <MdThumbUp size='32px' /> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
                  </>}
       </a>
