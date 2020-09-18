aa024be485e7457582362133610c91f5cc496d91
Alejandro Medina <5almero5@gmail.com>
2020-09-18 15:06:06 -0500

    Add like on enpoint

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ImgWrapper, Img, Article } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+import { FavButton } from '../FavButton'
+
+import { gql } from 'apollo-boost'
+import { useMutation } from '@apollo/react-hooks'
+
+// Import const TOGGLE_LIKE
+const TOGGLE_LIKE = gql`
+  mutation likeAnonymousPhoto($input: LikePhoto!) {
+    likeAnonymousPhoto(input: $input) {
+      id
+      liked
+      likes
+    }
+  }`
 
 const DEFAULT_CARD = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,19 +23,25 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_CARD }) => {
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
   const [show, ref] = useNearScreen()
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const [toggleLike] = useMutation(TOGGLE_LIKE, {
+    variables: { input: { id } }
+  })
+
+  const handleFavClick = () => {
+    setLiked(!liked)
+    toggleLike()
+  }
   return (
     <Article ref={ref}>
-      <a href={`/?detail=${id}`}>
-        {show && <>
-          <ImgWrapper>
-            <Img src={src} />
-          </ImgWrapper>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+      {show &&
+        <>
+          <a href={`/?detail=${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
         </>}
-      </a>
     </Article>
   )
 }
