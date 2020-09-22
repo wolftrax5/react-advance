e7bc0185d7e9cb92d9d2c63f9ee535ca5959bd81
Alejandro Medina <5almero5@gmail.com>
2020-09-21 19:29:39 -0500

    Add Auth route validations

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Router } from '@reach/router'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,21 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+                </Router>
+              : <Router>
+                <NotRegisterUser path='/favs' />
+                <NotRegisterUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotRegisterUser = () => (
+  <h1>NotRegisterUser</h1>
+)
